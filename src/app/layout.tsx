35603b3625ae330c3db47fbe327ac708073bdf07
Artem Ganeyev <kainar_malay@Artems-MacBook-Air.local>
2025-10-01 22:04:55 +0500

    f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Meme Generator",
-  description: "Создайте свой собственный мем!",
+  description: "Are you okay????!",
 };
 
 export default function RootLayout({
